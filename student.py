bca82d79bf02d7d4235195e3b9f9d230585b439f
prashanthmadhala <36609788+prashanthmadhala@users.noreply.github.com>
2018-12-03 16:25:09 +0200

    Update student.py

--- student.py
+++ student.py
@@ -9,7 +9,13 @@ import pandas as pd
 import numpy as np 
 from pandas import ExcelWriter
 import csv
-bf = pd.read_csv('C:\\Users\prash\\Downloads\\GitHub\\blackfriday-analysis\\BlackFriday.csv')
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+#path = is the path of the csv file or the dataset
+path = 'C:\\Users\prash\\Downloads\\GitHub\\blackfriday-analysis\\BlackFriday.csv'
+bf = pd.read_csv(path)
 
 #sorting by marital status as married
 n = bf.where(bf['Marital_Status']==1)
@@ -32,4 +38,24 @@ n1 = n1.dropna(axis=0, how='all')
 
 des = n1.describe()
 
+#saving the file to a csv
 """n1.to_csv("C:\\Users\\prash\\Downloads\\bf1825.csv",index=False,header = True, sep=",")"""
+
+
+#plotting
+sns.set(color_codes=True)
+
+x = n1['Purchase']
+
+#sns.distplot(x)
+
+mean = np.mean(x)
+median = np.median(x)
+
+#plotting the histogram of the purchase 
+sns.distplot(x, bins = 10, kde=False, rug=True)
+
+"""We can find that the distribution is a left skewed distribution where 
+mean (11908.914) and median(11886.5) are close to each other"""
+
+
